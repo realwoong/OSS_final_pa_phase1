0f675a418db10c838f04c05273c8a22cf69c341a
realwoong <woooong@skku.edu>
2024-06-05 14:22:58 +0900

    item 시스템 추가 : item_gun(관통) 추가

--- finalpa.py
+++ finalpa.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # 초기화
 pygame.init()
@@ -15,6 +16,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
 
 # 폰트 설정
 font = pygame.font.Font(None, 74)
@@ -24,12 +26,19 @@ small_font = pygame.font.Font(None, 36)
 heart_image = pygame.image.load('images/heart.png')
 heart_image = pygame.transform.scale(heart_image, (20, 20))  # 하트 이미지 크기 조정
 
+# 아이템 이미지 로드
+item_images = {
+    'item_gun': pygame.transform.scale(pygame.image.load('images/item_gun.png'), (20, 20))
+}
+
 # 발판
 paddle_width = 100
 paddle_height = 10
 paddle_x = (screen_width - paddle_width) // 2
 paddle_y = screen_height - 30
 paddle_speed = 10
+paddle_color = BLUE
+paddle_gun_active = False
 
 # 공
 ball_radius = 10
@@ -37,6 +46,7 @@ ball_x = screen_width // 2
 ball_y = screen_height // 2 - 50
 ball_speed_x = 0
 ball_speed_y = 0
+ball_piercing = False
 
 # 벽돌
 brick_width = 75
@@ -47,6 +57,14 @@ for i in range(6):
         brick_x = 20 + j * (brick_width + 10)
         brick_y = 20 + i * (brick_height + 10)
         bricks.append(pygame.Rect(brick_x, brick_y, brick_width, brick_height))
+
+# 아이템 리스트 초기화
+items = []
+
+# 아이템 확률
+# item_drop_chance = 0.3  # 30%
+item_drop_chance = 1.0  # for test, 수정예정
+item_types = list(item_images.keys())  # 아이템 종류 목록
 
 # 게임 상태
 game_active = False
@@ -106,6 +124,9 @@ while running:
                     ball_y = paddle_y - ball_radius
                     ball_speed_x = 0
                     ball_speed_y = 0
+                    paddle_color = BLUE
+                    paddle_gun_active = False
+                    ball_piercing = False
                     start_ticks = pygame.time.get_ticks()  # 시간 초기화
                     bricks = []
                     for i in range(6):
@@ -129,6 +150,9 @@ while running:
                 ball_y = paddle_y - ball_radius
                 ball_speed_x = 0
                 ball_speed_y = 0
+                paddle_color = BLUE
+                paddle_gun_active = False
+                ball_piercing = False
                 start_ticks = pygame.time.get_ticks()  # 시간 초기화
                 bricks = []
                 for i in range(6):
@@ -144,6 +168,9 @@ while running:
                 ball_y = paddle_y - ball_radius
                 ball_speed_x = 0
                 ball_speed_y = 0
+                paddle_color = BLUE
+                paddle_gun_active = False
+                ball_piercing = False
                 start_ticks = pygame.time.get_ticks()  # 시간 초기화
         if event.type == pygame.KEYDOWN:
             if game_active and ball_speed_y == 0:
@@ -181,12 +208,24 @@ while running:
             # 공이 패들의 왼쪽에 맞으면 왼쪽으로, 오른쪽에 맞으면 오른쪽으로 튕겨나감
             hit_pos = ball_x - paddle_x  # 패들에 맞은 위치
             ball_speed_x = (hit_pos - paddle_width / 2) / (paddle_width / 2) * 5  # 속도 조정
+            # 패들에 아이템 효과가 적용된 경우
+            if paddle_gun_active:
+                ball_piercing = True
+                paddle_gun_active = False
+                paddle_color = BLUE
 
         # 공이 벽돌에 부딪히면 벽돌 제거 및 방향 전환
         for brick in bricks[:]:
             if brick.collidepoint(ball_x, ball_y):
                 bricks.remove(brick)
-                ball_speed_y = -ball_speed_y
+                if not ball_piercing:
+                    ball_speed_y = -ball_speed_y
+                # 아이템 드랍 확률 계산
+                if random.random() < item_drop_chance:
+                    item_x = brick.x + (brick.width - 20) // 2  # 아이템이 중앙에서 떨어지게 조정
+                    item_y = brick.y
+                    item_type = random.choice(item_types)  # 아이템 타입 랜덤 선택
+                    items.append({'rect': pygame.Rect(item_x, item_y, 20, 20), 'type': item_type})
                 break
 
         # 추가: 공이 바닥에 닿으면 라이프 감소
@@ -200,6 +239,7 @@ while running:
                 ball_y = paddle_y - ball_radius
                 ball_speed_x = 0
                 ball_speed_y = 0
+                ball_piercing = False  # 공의 관통 효과 초기화
 
         # 벽돌을 모두 제거했을 때 라운드 클리어
         if not bricks:
@@ -207,7 +247,7 @@ while running:
             round_clear = True
 
         # 발판 추가
-        pygame.draw.rect(screen, BLUE, (paddle_x, paddle_y, paddle_width, paddle_height))
+        pygame.draw.rect(screen, paddle_color, (paddle_x, paddle_y, paddle_width, paddle_height))
 
         # 공 추가
         pygame.draw.circle(screen, RED, (ball_x, ball_y), ball_radius)
@@ -215,6 +255,21 @@ while running:
         # 벽돌 추가
         for brick in bricks:
             pygame.draw.rect(screen, WHITE, brick)
+
+        # 아이템의 위치 업데이트 및 패들과의 충돌 처리
+        for item in items[:]:
+            item['rect'].y += 4  # 아이템 낙하 속도
+            if item['rect'].y > screen_height:
+                items.remove(item)
+            elif paddle_y < item['rect'].y + 20 < paddle_y + paddle_height and paddle_x < item['rect'].x < paddle_x + paddle_width:
+                items.remove(item)
+                if item['type'] == 'item_gun':
+                    paddle_color = PURPLE
+                    paddle_gun_active = True
+
+        # 아이템 추가
+        for item in items:
+            screen.blit(item_images[item['type']], item['rect'])
 
         # 경과 시간 계산 및 표시
         seconds = (pygame.time.get_ticks() - start_ticks) // 1000
